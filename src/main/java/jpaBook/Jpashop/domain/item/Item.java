b0d86985ecd19394f3154fea242c3373c2f127b6
haeun1700 <haeun7173@gmail.com>
2025-03-22 11:22:37 +0900

    #6: 아이템 엔티티에 카테고리 관계 연결

--- src/main/java/jpaBook/Jpashop/domain/item/Item.java
+++ src/main/java/jpaBook/Jpashop/domain/item/Item.java
@@ -1,8 +1,12 @@
 package jpaBook.Jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpaBook.Jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 관계형 DB는 상속이 없어 전략을 사용한다. (-> 싱글테이블 전략 사용)
@@ -17,4 +21,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }

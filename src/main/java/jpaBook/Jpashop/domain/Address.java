26811b57743465dad1a45f05b824a475439f3b33
haeun1700 <haeun7173@gmail.com>
2025-03-22 11:19:53 +0900

    #6: 값 타입으로 Setter삭제, 기본 생성자 추가

--- src/main/java/jpaBook/Jpashop/domain/Address.java
+++ src/main/java/jpaBook/Jpashop/domain/Address.java
@@ -1,14 +1,20 @@
 package jpaBook.Jpashop.domain;
 
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Entity;
 import lombok.Getter;
-import lombok.Setter;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {}
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }

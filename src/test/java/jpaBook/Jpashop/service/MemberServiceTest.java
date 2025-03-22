153856186f411908b085156b4bda07909f73f66d
haeun1700 <haeun7173@gmail.com>
2025-03-22 15:10:30 +0900

    #9: 회원가입 테스트 구현

---
+++ src/test/java/jpaBook/Jpashop/service/MemberServiceTest.java
@@ -0,0 +1,44 @@
+package jpaBook.Jpashop.service;
+
+import jpaBook.Jpashop.domain.Member;
+import jpaBook.Jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class) // junit4에서만 사용, junit5버전에서 사용안해도 가능
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false) // 쿼리까지 확인 가능
+    public void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("Kim");
+        // when
+        Long saveId = memberService.join(member);
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+
+        // when
+
+        //then
+    }
+
+}
